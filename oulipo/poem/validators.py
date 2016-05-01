68ab76de56d1c5fa4243c05ebdf650cf6223310d
Sam Whitehall <me@samwhitehall.com>
2016-05-01 16:39:06 +0100

    [api] add todos for validation

---
+++ oulipo/poem/validators.py
@@ -0,0 +1,13 @@
+"""
+- Options
+    * advance_by keys are PoS
+        * check correct range of value
+
+- Token
+    * category is an appropriate value
+    * if content exists and it's a PoS, there should be an original
+    * newline
+
+- Poem
+    * raw_text should be consistent with the tokens
+"""
