e330a035134bd98ef27de037c76e501a3fb053b3
Sam Whitehall <me@samwhitehall.com>
2016-06-14 22:22:10 +0100

    [django] update url

--- oulipo/poem/urls.py
+++ oulipo/poem/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from poem.views import PoemViewSet
 
 router = routers.SimpleRouter()
-router.register(r'poems', PoemViewSet, base_name='poems')
+router.register(r'poem', PoemViewSet, base_name='poem')
 
 urlpatterns = router.urls
