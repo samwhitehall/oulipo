a2af0116f21d65aeec589948013ee45e619d6b2e
Sam Whitehall <me@samwhitehall.com>
2016-06-22 15:23:26 +0100

    [frontend] add Gruntfile

---
+++ oulipo/frontend/Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    // configure grunt
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // js linting
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            build: ['js/*.js']
+        },
+
+        // js minification & concatenation
+        concat: {
+            options: {
+                banner: '/* <%= pkg.name %> v<%= pkg.version %> by <%= pkg.author %> */\n'
+            },
+            build: {
+                src: [
+                    'js/vendor/jquery.*.js', 
+                    'js/vendor/underscore.*.js', 
+                    'js/vendor/backbone.*.js', 
+                    'js/vendor/backbone-deep-model.*.js', 
+                    'js/app.js', 
+                    'js/models.js', 
+                    'js/views.js', 
+                    'js/setup.js'
+                ],
+                dest: 'oulipo.js'
+            }
+        },
+        
+        // configure watch for auto-updating
+        watch: {
+            script: {
+                files: 'js/**/*.js',
+                tasks: ['jshint', 'concat']
+            }
+        }
+
+    });
+
+    // load grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // register tasks
+    grunt.registerTask('default', ['jshint', 'concat']);
+};
