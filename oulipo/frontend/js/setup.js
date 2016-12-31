6be04679671c534399cf72a6130a75da9f130780
Sam Whitehall <me@samwhitehall.com>
2016-12-31 15:58:52 +0000

    [frontend] setup default poem & options

--- oulipo/frontend/js/setup.js
+++ oulipo/frontend/js/setup.js
@@ -1,18 +1,38 @@
 app.poem = new app.models.Poem({
-    'title': 'Common People',
-    'options': {
-        'advance_by__verb': 0,    
-        'advance_by__noun': 0    
-    },
-    'raw_text': 'she came from Greece, she had a thirst for knowledge,\n' +
-                'she studied sculpture at St Martin\'s College,\n' +
-                'that\'s where I caught her eye'
+    'title': 'The Road Not Taken',
+    'raw_text': 'Two roads diverged in a yellow wood,\n' + 
+                'And sorry I could not travel both\n' + 
+                'And be one traveler, long I stood\n' + 
+                'And looked down one as far as I could\n' + 
+                'To where it bent in the undergrowth;\n' + 
+                '\n' + 
+                'Then took the other, as just as fair,\n' + 
+                'And having perhaps the better claim,\n' + 
+                'Because it was grassy and wanted wear;\n' + 
+                'Though as for that the passing there\n' + 
+                'Had worn them really about the same,\n' + 
+                '\n' + 
+                'And both that morning equally lay\n' + 
+                'In leaves no step had trodden black.\n' + 
+                'Oh, I kept the first for another day!\n' + 
+                'Yet knowing how way leads on to way,\n' + 
+                'I doubted if I should ever come back.\n' + 
+                '\n' + 
+                'I shall be telling this with a sigh\n' + 
+                'Somewhere ages and ages hence:\n' + 
+                'Two roads diverged in a wood, and I—\n' + 
+                'I took the one less traveled by,\n' + 
+                'And that has made all the difference.'
 });
 
 app.poemView = new app.views.PoemView();
 app.optionsView = new app.views.OptionsView({
     model: app.poem.get('options')
 });
+
+// set default advance_by (noun: -2, verb: 3)
+app.poem.get('options').set('advance_by__noun', 1);
+app.poem.get('options').set('advance_by__verb', -3);
 
 app.poem.on('sync', app.poemView.reset, app.poemView);
 app.optionsView.render();
